f00970c4a398439a3ec0042a2ebbf8754dba8f72
Naresh Darelli <nareshdarelli3@gmail.com>
2026-06-09 23:59:03 +0530

    Updating Two sum problem solution with Hashmap

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ def twosum(nums: list[int], target: int) -> list[int]:
     return []
 
 print(twosum([2, 7, 11, 15], 9))
+
+#Two sum with hashmap
+def twosum(nums: list[int], target: int) -> list[int]:
+    hashmap = {}
+    for i, num in enumerate(nums):
+        num2 = target - num
+        if num2 in hashmap:
+            return [hashmap[num2], i]
+        hashmap[num] = i
+
+print(twosum([2, 7, 11, 15], 9))
