c24f2269f4cf8631046f574fd8bfc6b0400dc235
Naresh Darelli <nareshdarelli3@gmail.com>
2026-06-21 16:10:46 +0530

    Revert "3 sum prob"
    This reverts commit 1828cd5f19c430ce043c1afc6cb467d41e2cd7a6.

--- two_sum.py
+++ two_sum.py
@@ -1,22 +1,3 @@
-def three_sum_brute_force(nums: List[int]) -> List[List[int]]:
-    """
-    Check every triplet of indices, dedupe with a set.
-    
-    Time:  O(n^3) - three nested loops over all triplets
-    Space: O(n)   - to store unique triplets (using a set of sorted tuples)
-    """
-    n = len(nums)
-    triplets = set()
-    
-    for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
-                    triplets.add(triplet)
-    
-    return [list(t) for t in triplets]
-
 #Two sum with brute force
 def twosum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):
