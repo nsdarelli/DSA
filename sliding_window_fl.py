ccaeed88ec6b5431d7a7f94d60e74fd7b32f315a
Naresh Darelli <nareshdarelli3@gmail.com>
2026-06-24 20:15:31 +0530

    sliding window fixed length and variable len

--- sliding_window_fl.py
+++ sliding_window_fl.py
@@ -28,4 +28,20 @@ def findMaxAvg(nums: list[int], k: int) -> float:
 
 print(findMaxAvg([1, 12, -5, -6, 50, 3], 4))
 
+def findMaxAvg(nums: list[int], k: int) -> float:
+    n = len(nums)
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+
+    for i in range(k, n):
+        window_sum += nums[i] - nums[i-k]
+        if window_sum > max_sum:
+            max_sum = window_sum
+
+    max_avg = max_sum / k
+
+    return max_avg
+
+print(findMaxAvg([1, 12, -5, -6, 50, 3], 4))
+
 
--- sliding_window_vl.py
+++ sliding_window_vl.py
@@ -27,3 +27,32 @@ def longest_substring_(s: str) ->int:
     return max_len
 
 print(longest_substring_("abcdabdcbb"))
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+def maxSubArray(nums):
+    n = len(nums)
+    max_sum = 0
+    for i in range(n):
+        cur_sum = 0
+        for j in range(i, n):
+            cur_sum+=nums[j]
+            if cur_sum>max_sum:
+                max_sum = cur_sum
+    return max_sum
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+def maxSubArray(nums):
+    n=len(nums)
+    max_sum = float('-inf')
+    cur_sum = 0
+
+    for i in range(n):
+        cur_sum += nums[i]
+        if cur_sum>max_sum:
+            max_sum = cur_sum
+        if cur_sum<0:
+            cur_sum = 0
+    return max_sum
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
